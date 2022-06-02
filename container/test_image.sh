2dfdeb42f6c3bff7f52ea96acce5ab0de9028136
pryce-turner <pryce.turner@gmail.com>
2022-06-03 11:51:52 +1200

    Fixed Snakefile to actually run correctly, up to call_variants, rebased image onto mamba instead of miniconda, changed dataset structure to include a folder for unsplit alignments, added more informative output to test script when testing running container

--- container/test_image.sh
+++ container/test_image.sh
@@ -4,7 +4,7 @@ podman build -f Containerfile -t coacervate:testing
 
 echo ""
 echo "Running new image.."
-podman run --rm -it localhost/coacervate:testing
+podman run --rm -it localhost/coacervate:testing snakemake --help
 
 echo "Cleaning up.."
 podman rmi localhost/coacervate:testing
