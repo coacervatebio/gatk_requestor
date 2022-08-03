8eac6ddd708c602ffa07ce7907d36fba1ddc3749
pryce-turner <pryce.turner@gmail.com>
2022-08-03 13:26:40 +1200

    Big clean up, refactor to better suit future direction

--- golem/haplotype_caller/no_ref_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ALIGN_IN=$1
-REG=$2
-VCF_OUT=$3
-
-echo "Running HaplotypeCaller.."
-java -jar /home/gatk-local.jar HaplotypeCaller --version
--- golem/haplotype_caller/simple_test.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path, PurePath
-from requestor import data
-
-als_path = Path('/home/vagrant/host_shared/snakemake/results/2_sample/alignments')
-
-for task in data(als_path):
-    for k,v in task.data.items():
-        print(k, ':', v)
-    break
