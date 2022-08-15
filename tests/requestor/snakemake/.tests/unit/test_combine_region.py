e4090ccd1e277766a92c3f6b7e96a168888f25ea
pryce-turner <pryce.turner@gmail.com>
2022-08-16 11:09:23 +1200

    Added snakemake auto-tests, renamed default hc_golem.smk in rules to workflow/Snakefile to match official structure

---
+++ tests/requestor/snakemake/.tests/unit/test_combine_region.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_combine_region():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/combine_region/data")
+        expected_path = PurePosixPath(".tests/unit/combine_region/expected")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+
+        # dbg
+        print("/mnt/results/combi_out/chr21_database", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "/mnt/results/combi_out/chr21_database",
+            "-f", 
+            "-j1",
+            "--keep-target-files",
+    
+            "--directory",
+            workdir,
+        ])
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
