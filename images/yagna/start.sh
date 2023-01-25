d95274c9c5b7247fa5a19fa06bf4bfe49de5fe0e
Pryce Turner <pryce.turner@gmail.com>
2023-01-26 10:57:30 +1300

    Refactored yagna as a standalone

--- images/yagna/start.sh
+++ images/yagna/start.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
+set -m
 # Start the yagna daemon and put it in the background
 # Using `-v yagna_datadir:/home/coacervate/.local/share/yagna`
 # will persist yagna datadir between containers
 echo "Starting yagna daemon.."
+yagna -d /coacervate/yagna service run &
+sleep 20
 
 get_appkey () {
-    echo $(yagna app-key list --json | jq -r .[0].key)
+    echo $(yagna -d /coacervate/yagna app-key list --json | jq -r .[0].key)
 }
 
 if [ -z $(get_appkey) ]
@@ -16,10 +19,10 @@ then
 elif [ $(get_appkey) = "null" ]
 then
     echo "No appkey found, creating requestor.."
-    yagna app-key create requestor
+    yagna -d /coacervate/yagna app-key create requestor
     sleep 5
     echo "Funding.."
-    yagna payment fund
+    yagna -d /coacervate/yagna payment fund
     sleep 10
 else
     echo "Found existing appkey"
@@ -28,4 +31,6 @@ fi
 echo "Exporting app key and initializing yagna as sender.."
 export YAGNA_APPKEY=$(get_appkey)
 sleep 3
-yagna payment init --sender
+yagna -d /coacervate/yagna payment init --sender
+
+fg %1
