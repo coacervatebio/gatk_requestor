25929cfb2519507fe150b6d8cedd4699771aa561
Pryce Turner <pryce.turner@gmail.com>
2023-02-17 11:58:57 +1300

    Flattened test asset dir structure, brought the checking files away from docker paradigm (runners etc) and simplified significantly, added Dockerfile for test-checker image

--- images/test_runner/checker/checkers.py
+++ images/test_runner/checker/checkers.py
@@ -1,7 +1,10 @@
+import logging
 import subprocess as sp
 import pysam as ps
 
+LOGGER = logging.getLogger(__name__)
 
+# Checkers
 class SimpleChecker:
     """Byte for byte file comparison checker using `cmp`"""
 
@@ -21,3 +24,23 @@ class VcfChecker:
         g_recs = [str(r) for r in gv.fetch()]
         e_recs = [str(r) for r in ev.fetch()]
         assert g_recs == e_recs
+
+# Utils
+def allowed_pattern(tf):
+    """
+    Filter function to remove files from comparison matching certain patterns
+    identifying files that change from run-to-run. E.g. failing comparison
+    because the timestamp is different.
+
+    :param tf: Target file being evaluated
+    :returns False: File contains failure pattern
+    :returns True: File does not contain failure pattern
+    """
+    expected_cmp_fail_patterns = ["__", "vcfheader.vcf", "gz.tbi"]
+
+    s_tf = str(tf)
+    for ecfp in expected_cmp_fail_patterns:
+        if ecfp in s_tf:
+            LOGGER.warn(f"Excluding {s_tf} for containing {ecfp}")
+            return False
+    return True
--- images/test_runner/checker/common.py
+++ images/test_runner/checker/common.py
@@ -2,8 +2,6 @@ import os
 import shutil
 import logging
 from pathlib import Path
-from docker.errors import DockerException, ContainerError, APIError, NotFound
-from tests.config import test_name, default_tmp, unit_assets_root
 
 LOGGER = logging.getLogger(__name__)
 
@@ -16,30 +14,24 @@ class ContainerTester:
 
     def __init__(
         self,
-        runner,
         checker,
         datadir,
-        tmpdir=default_tmp,
+        tmpdir='',
         track_unexpected=True,
     ):
         """
-        :param runner: Component Runner class for running container
-        :type runner: _type_
         :param checker: Component Checker class for comparing outputs/expected
         :type checker: _type_
         :param datadir: Directory holding mock assets and expected outputs
         :type datadir: PurePath
-        :param tmpdir: Directory for holding temporary outputs, defaults to Path("assets/TEMP/")
-        :type tmpdir: Path
         :param track_unexpected: Set to False if there are output files not relevant to testing, defaults to True
         :type track_unexpected: Bool
         """
         LOGGER.info(
-            f"Initializing ContainerTester with {runner}, {checker}, and {datadir}"
+            f"Initializing tester with {checker}, and {datadir}"
         )
         self.datadir = datadir
         self.tmpdir = tmpdir
-        self.runner = runner
         self.checker = checker
         self.track_unexpected = track_unexpected  # can be set to False when ignoring inputs that change with every run
 
@@ -56,16 +48,6 @@ class ContainerTester:
             for f in files
         )
         LOGGER.debug(f"Determined target files as: {self.target_files}")
-
-        # Sometimes target_str needs to be overwritten, e.g when it
-        # is a dir producing many unspecified targets
-        self.runner.target_string = " ".join([f"/{f}" for f in self.target_files])
-
-        # Setup default mounts that can be overridden before calling run()
-        self.runner.vols = [
-            f"{str(unit_assets_root.joinpath('test_config.yml'))}:/data/config/config.yml",
-            f"{str(self.tmpdir.joinpath('data', 'results').resolve())}:/data/results"
-        ]
 
     def check_files(self):
         """Iterate through inputs and outputs, comparing and raising issues where appropriate
@@ -128,78 +110,3 @@ class ContainerTester:
                 )
             )
 
-    def clean_con(self):
-        """Clean up container created by Runner"""
-        try:
-            test_con = self.runner.cons.get(test_name)
-            LOGGER.info(f"Cleaning up {test_con}")
-            test_con.remove(force=True)
-        except NotFound:
-            pass  # Nothing to clean up
-
-    def clean_tmp(self):
-        """Remove temporary directory used for test inputs/outputs"""
-        LOGGER.info(f"Cleaning up tmpdir: {self.tmpdir}")
-        if self.tmpdir.is_dir():
-            shutil.rmtree(self.tmpdir)
-
-    def run(self, run_con=True, check=True, clean_con=True, clean_tmp=True):
-        """Main entrypoint for running defined test case, handling errors and cleaning up.
-
-        :param run_con: Call Runner's run() method, defaults to True
-        :type run_con: bool, optional
-        :param check: Check output of container run, defaults to True
-        :type check: bool, optional
-        :param clean_con: Remove container after test, set to False to manually run things against container after test exits, defaults to True
-        :type clean_con: bool, optional
-        :param clean_tmp: Remove temp dir, set to False to manually inspect inputs/outputs, defaults to True
-        :type clean_tmp: bool, optional
-        :raises ce: ContainerError, logs issues with container process
-        :raises ae: APIError, logs issues with Docker daemon
-        :raise de: DockerException, catch-all log for Docker issue
-        """
-        LOGGER.debug("Entering run() method")
-        try:
-            if run_con:
-                self.runner.run()
-            if check:
-                self.check_files()
-        except ContainerError as ce:
-            LOGGER.critical("Container exited with non-zero code")
-            raise ce
-        # Catches errors relating to getting logs from a container that never started
-        except APIError as ae:
-            LOGGER.critical("Docker daemon returned an error")
-            raise ae
-        # Catch any docker SDK issues
-        except DockerException as de:
-            LOGGER.critical("Unhandled Docker exception")
-            raise de
-        finally:
-            if clean_con:
-                self.clean_con()
-            if clean_tmp:
-                self.clean_tmp()
-
-
-# Small Utils
-
-
-def allowed_pattern(tf):
-    """
-    Filter function to remove files from comparison matching certain patterns
-    identifying files that change from run-to-run. E.g. failing comparison
-    because the timestamp is different.
-
-    :param tf: Target file being evaluated
-    :returns False: File contains failure pattern
-    :returns True: File does not contain failure pattern
-    """
-    expected_cmp_fail_patterns = ["__", "vcfheader.vcf", "gz.tbi"]
-
-    s_tf = str(tf)
-    for ecfp in expected_cmp_fail_patterns:
-        if ecfp in s_tf:
-            LOGGER.warn(f"Excluding {s_tf} for containing {ecfp}")
-            return False
-    return True
